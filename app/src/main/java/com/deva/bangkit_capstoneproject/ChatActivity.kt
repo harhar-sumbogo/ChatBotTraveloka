de74522e59b7821ec969864c079164c5ee903d7a
DevaPutra11 <malfrizaldevas@gmail.com>
2022-05-15 20:15:04 +0700

    Adding logo appbar in login activity, add bot picture in chat activity app bar, add edit text animation in login activity and register activity

--- app/src/main/java/com/deva/bangkit_capstoneproject/ChatActivity.kt
+++ app/src/main/java/com/deva/bangkit_capstoneproject/ChatActivity.kt
@@ -7,5 +7,18 @@ class ChatActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_chat)
+
+        supportActionBar?.apply {
+            title = "  Traveloka Bot"
+            setDisplayHomeAsUpEnabled(true)
+            setDisplayShowHomeEnabled(true)
+            setDisplayUseLogoEnabled(true)
+            setLogo(R.drawable.traveloka_chatbot_logo_30)
+        }
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        onBackPressed()
+        return super.onSupportNavigateUp()
     }
 }
--- app/src/main/java/com/deva/bangkit_capstoneproject/MainActivity.kt
+++ app/src/main/java/com/deva/bangkit_capstoneproject/MainActivity.kt
@@ -3,6 +3,8 @@ package com.deva.bangkit_capstoneproject
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import com.deva.bangkit_capstoneproject.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -14,6 +16,13 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        supportActionBar?.apply {
+            setDisplayShowTitleEnabled(false)
+            setDisplayShowHomeEnabled(true)
+            setDisplayUseLogoEnabled(true)
+            setLogo(R.drawable.logo_traveloka)
+        }
+
         binding.btnToLogin.setOnClickListener {
             val i = Intent(this,LoginActivity::class.java)
             startActivity(i)
@@ -22,6 +31,23 @@ class MainActivity : AppCompatActivity() {
         binding.btnToRegister.setOnClickListener {
             val i = Intent(this,RegisterActivity::class.java)
             startActivity(i)
+        }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        val inflater = menuInflater
+        inflater.inflate(R.menu.option_menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.menu_chat -> {
+                val i = Intent(this,ChatActivity::class.java)
+                startActivity(i)
+                return true
+            }
+            else -> return true
         }
     }
 }
