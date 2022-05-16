3e70818c5043c5369b1d4acb131d73d901e9a402
harhar-sumbogo <harris.kurniadi@ti.ukdw.ac.id>
2022-05-16 13:33:13 +0700

    Fixing UI Crash on Login Activity

--- app/src/main/java/com/deva/bangkit_capstoneproject/LoginActivity.kt
+++ app/src/main/java/com/deva/bangkit_capstoneproject/LoginActivity.kt
@@ -2,11 +2,15 @@ package com.deva.bangkit_capstoneproject
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.deva.bangkit_capstoneproject.databinding.ActivityLoginBinding
 
 class LoginActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityLoginBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
         supportActionBar?.hide()
     }
