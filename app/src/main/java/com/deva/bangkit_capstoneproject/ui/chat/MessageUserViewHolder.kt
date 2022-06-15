6e6a7ce01562e3e40a009e5ea272fe162366a814
harhar-sumbogo <harris.kurniadi@ti.ukdw.ac.id>
2022-06-15 22:39:15 +0700

    Fix bug user timestamp

--- app/src/main/java/com/deva/bangkit_capstoneproject/ui/chat/MessageUserViewHolder.kt
+++ app/src/main/java/com/deva/bangkit_capstoneproject/ui/chat/MessageUserViewHolder.kt
@@ -3,15 +3,14 @@ package com.deva.bangkit_capstoneproject.ui.chat
 import android.text.format.DateUtils
 import androidx.recyclerview.widget.RecyclerView
 import com.deva.bangkit_capstoneproject.databinding.CardMessageUserBinding
-import com.deva.bangkit_capstoneproject.core.data.remote.response.Message
 import com.deva.bangkit_capstoneproject.core.domain.model.MessageModel
 
 class MessageUserViewHolder(private val binding: CardMessageUserBinding) : RecyclerView.ViewHolder
     (binding.root) {
     fun bind(message: MessageModel) {
-        binding.message.text = message.message
-//        if (message.timestamp != null)  {
-//            binding.timestamp.text = DateUtils.getRelativeTimeSpanString(message.timestamp)
-//        }
+        with(message) {
+            binding.message.text = this.message
+            binding.timestamp.text = DateUtils.getRelativeTimeSpanString(this.id.toLong())
+        }
     }
 }
